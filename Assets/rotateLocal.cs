c69f43c39d2a223fd83b6167e0af76b2b0ba8e6c
Alexander Simonov <alexandrsiminov@gmail.com>
2017-10-18 16:36:04 +0300

    bug fix...

--- Assets/rotateLocal.cs
+++ Assets/rotateLocal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class rotateLocal : MonoBehaviour {
 
-    private float speed = 220;
+    private float speed = 120;
     private float angle = 0;
 
     public bool notChange = false;
@@ -17,12 +17,11 @@ public class rotateLocal : MonoBehaviour {
         this.angle = angle;
     }
 
-    private void Update() {
+    private void LateUpdate() {
         if (!notChange) {
-            //Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 0), Camera.main.WorldToScreenPoint(new Vector3(0, transform.position.y, 0)));
             transform.position = new Vector3(transform.position.x, transform.position.y, (Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y, 0)).y / 1000));
-
         }
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), speed * Time.deltaTime);     
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), speed * Time.deltaTime);
     }
 }
