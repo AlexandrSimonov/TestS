3f20901980e7230b8bdb0f63ca899c955d76218a
Alexander Simonov <alexandrsiminov@gmail.com>
2017-10-19 17:37:09 +0300

    Minimal UI and icons

---
+++ Assets/ControlActionsPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlActionsPanel : MonoBehaviour {
+
+    private Vector3 targetPosition;
+    private RectTransform transformPanel;
+    public GameObject button;
+
+    private bool show = false;
+
+    void Start() {
+        transformPanel = transform.GetComponent<RectTransform>();
+        targetPosition = transformPanel.position;
+    }
+
+    void Update() {
+        transformPanel.position = Vector3.MoveTowards(transformPanel.position, targetPosition, 4);
+
+        if (show) {
+            if (Input.GetMouseButtonDown(0)) {
+                HidePanel();
+            }
+           
+            button.SetActive(false);
+        } else {
+            button.SetActive(true);
+        }
+    }
+
+    public void HidePanel() {
+        targetPosition.x *= -1;
+        show = false;
+    }
+
+    public void ShowPanel() {
+        targetPosition.x *= -1;
+        show = true;
+    }
+}
