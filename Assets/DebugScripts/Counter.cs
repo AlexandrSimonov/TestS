b6905ab202456161cb0319d488c3166950f04100
Alexander Simonov <alexandrsiminov@gmail.com>
2017-11-22 20:39:52 +0200

    Simple counter

---
+++ Assets/DebugScripts/Counter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+    private RxField<int> counter = new RxField<int>(0);
+    private Text textCounter;
+
+    private void Awake() {
+        textCounter = GetComponent<Text>();
+
+        counter.OnChangeEvent.AddListener(View);
+    }
+
+    private void View(int value) {
+        textCounter.text = "" + value;
+    }
+
+    public void Minus() {
+        if (counter.Value >= 1) {
+            counter.Value--;
+        }
+    }
+
+    public void Plus() {
+        counter.Value++;
+    }
+}
