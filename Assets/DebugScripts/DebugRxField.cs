ece4568a817448266a3d01fa6d8be24e90f4cd48
Alexander Simonov <alexandrsiminov@gmail.com>
2017-11-22 17:04:55 +0200

    RxField and Debug RxField

---
+++ Assets/DebugScripts/DebugRxField.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugRxField : MonoBehaviour {
+
+    public RxField<int> num;
+
+    // Use this for initialization
+    void Start() {
+        num = new RxField<int>(2);
+        num.OnChangeEvent.AddListener(Changed);
+
+        num.Value = 1;
+
+        num.Value = 2;
+
+        num.Value = 3;
+    }
+
+    private void Changed(int num) {
+        Debug.Log(num);
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Utils/RxField.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Events;
+
+public class RxField<T> {
+    private T _value;
+    public RxEvent OnChangeEvent = new RxEvent();
+
+    public T Value {
+        get {
+            return _value;
+        }
+        set {
+            _value = value;
+            OnChangeEvent.Invoke(_value);
+        }
+    }
+
+    public RxField(T defaultValue) {
+        Value = defaultValue;
+    }
+
+    public class RxEvent : UnityEvent<T> { }
+}
