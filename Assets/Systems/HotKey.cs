5563f954b0e906da715ac26cdd9aed9771faa1fa
Alexander Simonov <alexandrsiminov@gmail.com>
2017-11-22 20:27:57 +0200

    Primitive HotKeySystem

---
+++ Assets/Systems/HotKey.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class HotKey {
+
+    public KeyCode hotkey;
+    public UnityEvent action;
+
+}
+
---
+++ Assets/Systems/HotKeySystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotKeySystem : MonoBehaviour {
+
+    public HotKey[] hotkeys;
+
+    private void Awake() {
+        Debug.Log("Проверочки хот кеев");
+    }
+
+    private void Update() {
+        foreach (HotKey hotkey in hotkeys) {
+            if (Input.GetKeyDown(hotkey.hotkey)) {
+                hotkey.action.Invoke();
+            }
+        }
+    }
+}
