30696f9d8c8ad82f8d90bb8f8cf1473240f3fd1c
Alexander Simonov <alexandrsiminov@gmail.com>
2017-11-22 22:28:51 +0200

    RxList

---
+++ Assets/DebugScripts/DebugRxList.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugRxList : MonoBehaviour {
+
+    private RxList<int> integers = new RxList<int>();
+
+    // Пока события работают нормально и можно с этим классом(после некоторого времени) переписать к примеру инвентарь
+    void Start() {
+        integers.OnAddEvent.AddListener(OnAdd);
+        integers.OnRemoveEvent.AddListener(OnRemove);
+
+        integers.Add(1);
+        integers.Add(2);
+        integers.Add(3);
+
+        integers.Remove(2);
+
+        Debug.Log("---Список---");
+
+        foreach (int i in integers) {
+            Debug.Log(i);
+        }
+    }
+
+    private void OnAdd(int item) {
+        Debug.Log("Add " + item);
+    }
+
+    private void OnRemove(int item) {
+        Debug.Log("Remove " + item);
+    }
+}
---
+++ Assets/Utils/RxList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class RxList<T> : List<T> {
+
+    public RxEvent OnAddEvent = new RxEvent();
+    public RxEvent OnRemoveEvent = new RxEvent();
+
+    public new void Add(T item) {
+        base.Add(item);
+
+        // И вот тут магия
+        OnAddEvent.Invoke(item);
+    }
+
+    public new void Remove(T item) {
+        base.Remove(item);
+
+        OnRemoveEvent.Invoke(item);
+    }
+
+    public class RxEvent : UnityEvent<T> { }
+}
