89e7670a3988d892a1f883fcf42374f62b166e47
Alexander Simonov <alexandrsiminov@gmail.com>
2017-11-23 11:36:53 +0200

    CoroutineTestFile

---
+++ Assets/DebugScripts/CoroutineTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineTest : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator PrintText() {
+        
+
+        yield return null;
+    }
+}
