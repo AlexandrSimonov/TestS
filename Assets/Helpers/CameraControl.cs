e867ba357aa0c65bebe00445ef04ff8a19cfd2a2
Alexander Simonov <alexandrsiminov@gmail.com>
2017-12-02 12:21:31 +0200

    Controllers player

--- Assets/Helpers/CameraControl.cs
+++ Assets/Helpers/CameraControl.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 
+// Не класс, а сплошное дерганье
 public class CameraControl : MonoBehaviour {
 
     public float speed = 1;
-    public float maxAcceleration = 1;
+    private float maxAcceleration = 10;
+
+
+    public Transform CameraRotateX; // Поворот по оси X
+    public Transform CameraRotateY; // Поворот по оси Y
+    public Transform CameraZ; // Отдаленность камеры
+    public Transform CameraY;
+
+    public Transform target = null;
+
     public enum CameraMode {
         Control,
         Follow
@@ -11,23 +21,108 @@ public class CameraControl : MonoBehaviour {
 
     public CameraMode cameraMode = CameraMode.Control;
 
+
+    private void Start() {
+        if (CameraRotateX == null) {
+            Debug.LogError("Не установлен transform CameraRotateX");
+            return;
+        }
+
+        if (CameraRotateY == null) {
+            Debug.LogError("Не установлен transform CameraRotateY");
+            return;
+        }
+
+        if (CameraZ == null) {
+            Debug.LogError("Не установлен transform CameraZ");
+            return;
+        }
+
+        if (CameraY == null) {
+            Debug.LogError("Не установлен transform CameraY");
+            return;
+        }
+    }
+    // Вот тут можно ограничить выход камеры за поле обзора
+
+
+    public void SetSpeed(float speed) {
+        this.speed = speed;
+    }
+
     public bool stop = false;
+
+    [ContextMenu("Контролировать")]
+    public void ControMode() {
+        cameraMode = CameraMode.Control;
+    }
+    [ContextMenu("Следовать")]
+    public void FollowMode() {
+        cameraMode = CameraMode.Follow;
+    }
+
+    public void Stop() {
+        stop = true;
+    }
+
+    public void Resume() {
+        stop = false;
+    }
 
     void Update() {
         if (!stop) {
             if (cameraMode == CameraMode.Control) {
-                Control();
+                //Control();
+
+                WarpOnControl();
             }
             if (cameraMode == CameraMode.Follow) {
-                Follow();
+                //Follow();
             }
+
+            Zoom();
         }
 
-        Zoom();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            stop = !stop;
+        }
+    }
+
+    private bool warping = false;
+    // Телепортация
+    private void WarpOnControl() {
+        if (target == null) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            warping = true;
+        }
+
+        if (warping) {
+            transform.position = new Vector3(target.position.x, 0, target.position.z);
+            CameraZ.localPosition = new Vector3(0, 0, -CameraDistance());
+            warping = false;
+        }
+    }
+
+    // Мы должны возвращать смещение для камеры, соответственно, нужно принимать камеру и 
+    private float CameraDistance() {
+        float y = CameraY.position.y; // Расстояние от камеры к игроку
+
+        float ctan = 1 / Mathf.Tan(CameraRotateX.localRotation.eulerAngles.x * (Mathf.PI / 180));
+
+        return ctan * y;
     }
 
     private void Follow() {
+        Vector3 pos = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 tar = new Vector3(target.position.x, 0, CameraDistance());
 
+        Vector3 targetCameraPosition = new Vector3(target.position.x, transform.position.y, CameraDistance());
+
+
+        transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition, 300);
     }
 
     private float border = 2;
@@ -39,6 +134,7 @@ public class CameraControl : MonoBehaviour {
     private Vector3 direction = new Vector3(0, 0, 0);
 
     // Сделать чтобы движение по диагонале, было со скоростью 0,7
+    // Нет поддержки тача
     private void Control() {
         if (Input.mousePosition.x - border <= 0) {
             direction += Vector3.left;
@@ -76,32 +172,36 @@ public class CameraControl : MonoBehaviour {
     public float minDistanceZoom = 0;
     public float maxDistanceZoom = 1;
     public float zoomSpeed = 1;
-
+    // Эта штука работает, но не работает с тачем нормально
     private void Zoom() {
         float y = transform.position.y;
         float scrollY = Input.mouseScrollDelta.y;
 
         // Условие того что не нужно двигаться
-        if (y < maxDistanceZoom && y > minDistanceZoom && scrollY == 0) {
+        if (y <= maxDistanceZoom && y >= minDistanceZoom && scrollY == 0) {
             return;
         }
         // Нам нужно притянуть y к maxDistance
         if (y > maxDistanceZoom) {
             Zooming(1, 5);
+            Debug.Log("Нужно приблизиться");
             return;
         }
         // Нам нужно притянуть y к minDistance
         if (y < minDistanceZoom) {
             Zooming(-1, 5);
+            Debug.Log("Нужно отдалиться");
             return;
         }
 
+
         Zooming(scrollY, zoomSpeed);
+        Debug.Log("Есть управление");
 
     }
 
     private void Zooming(float direction, float speed) {
-        Vector3 target = transform.position + Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).direction * direction;
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        /*Vector3 target = transform.position + Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).direction * direction;
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);*/
     }
 }
--- Assets/UnitLogic/PlayerController.cs
+++ Assets/UnitLogic/PlayerController.cs
@@ -8,12 +8,14 @@ public class PlayerController : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        
+
         agent = GetComponent<NavMeshAgent>();
-        //agent.updateRotation = false;
+        agent.updateRotation = false;
         //agent.updatePosition = false;
     }
 
+    private bool move = false;
+    private Vector3 direction;
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(1)) {
@@ -22,8 +24,33 @@ public class PlayerController : MonoBehaviour {
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100)) {
+                //direction = hit.point - transform.position;
                 agent.SetDestination(hit.point);
             }
+
+            move = true;
+            direction.y = 0;
+
+            direction.Normalize();
+
+            float angle = Mathf.Acos(Vector3.Dot(direction, transform.forward.normalized)) * 180 / Mathf.PI;
+
+            if (Mathf.Abs(angle) > 25) {
+                Debug.Log("Нужно остановиться и повернуться");
+            } else {
+                Debug.Log("Можно повернуться во время хотьбы");
+            }
         }
+
+        if (move) {
+            if (direction != Vector3.zero) {
+                //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), 150 * Time.deltaTime);
+            }
+            if (Vector3.Distance(new Vector3(agent.pathEndPosition.x, 0, agent.pathEndPosition.z), new Vector3(transform.position.x, 0, transform.position.z)) < 0.1f) {
+                Debug.Log("Мы пришли");
+                move = false;
+            }
+        }
+
     }
 }
